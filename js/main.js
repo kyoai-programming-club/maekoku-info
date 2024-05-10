a5f77eb4237d3973270c6c81afe015e9dddafe29
kyoai-programming-club <155413024+kyoai-programming-club@users.noreply.github.com>
2024-05-10 19:07:42 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,4 +12,43 @@
   close.addEventListener('click', () => {
     mask.classList.remove('mask-open');
   });
+
+  // エンジョイカルーセル
+  const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
+  const ul = document.querySelector('.enjoy-cal ul');
+  const slides = ul.children;
+  let currentIndex = 0;
+
+  function updateButtons() {
+    prev.classList.remove('enjoy-hidden');
+    next.classList.remove('enjoy-hidden');
+
+    if (currentIndex === 0) {
+      prev.classList.add('enjoy-hidden')
+    }
+    if (currentIndex === slides.length - 1) {
+      next.classList.add('enjoy-hidden')
+    }
+  }
+
+  updateButtons();
+
+  next.addEventListener('click', () => {
+    currentIndex++;
+    updateButtons();
+    const sliderWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1 * sliderWidth * currentIndex}px)`;
+  });
+
+  prev.addEventListener('click', () => {
+    currentIndex--;
+    updateButtons();
+    const sliderWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1 * sliderWidth * currentIndex}px)`;
+  });
+
+  window.addEventListener('resize', () => {
+    sliderWidth = slides[0].getBoundingClientRect().width;
+  });
 }
